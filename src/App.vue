d15afab5e6c01a764d347acb897ba4eeae5b0e94
Circuandante <circuandante@gmail.com>
2020-06-27 23:04:03 -0500

    add layout

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id='app'>
+    <div id='nav'>
+      <router-link to='/'>Home</router-link> |
+      <router-link to='/about'>About</router-link>
+    </div>
+    <router-view/>
+    <navigation-link url="/profile">
+      Su Perfil
+      <slot />
+    </navigation-link>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .mapa {
+    height: 425px;
+  }
+</style>
